06c8a7bc29b1b0b7043967745d983609e74a6aee
Aleph-Phoebe <phbwangui47@gmail.com>
2024-07-07 19:05:05 +0300

    DONE

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+
+// Input an ElementById
+const input = document.getElementById("button");
+  input.addEventListener("click", () => {
+    alert("I was clicked!");
+  })
 }
